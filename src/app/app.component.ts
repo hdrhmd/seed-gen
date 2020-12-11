f0fa90acb2121fb398303f4732adfda0c32f05c6
Hameed <hdrhmd@amazon.com>
2020-12-11 00:24:14 -0800

    added the logic using bip39 lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+const bip39 = require('bip39');
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'seed-gen';
+  diceRolls = [];
+  words = [];
+  dictionary = [];
+  validChecksums = [];
+
+  constructor() {
+    this.dictionary = bip39.wordlists.english;
+  }
+
+  displayRolls(rolls) {
+    return rolls.map(n => n + 1).join(', ');
+  }
+
+  addEntropy(num) {
+    this.diceRolls.push(num);
+    if (this.diceRolls.length === 4) {
+      const rolls = this.diceRolls;
+      const index = (rolls[0] % 4) * 512 + rolls[1] * 64 + rolls[2] * 8 + rolls[3];
+      this.words.push({
+        rolls,
+        index,
+        label: this.dictionary[index]
+      });
+      this.diceRolls = [];
+    }
+
+    if (this.words.length === 23) {
+      const entropyString = this.words.map(w => w.label).join(' ').concat(' ');
+      this.validChecksums = this.dictionary.filter(w => bip39.validateMnemonic(entropyString + w));
+    }
+  }
 }
